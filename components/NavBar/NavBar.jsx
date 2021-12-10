dff508813264484fb942a963378621f2943126c2
agusraschia <85563413+agusraschia@users.noreply.github.com>
2021-12-09 21:12:55 -0300

    Add files via upload
    Incorporacion de la carpeta components con el Navbar

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react' 
+
+function NavBar() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="">Listado</a>   
+                </li>
+                <li>
+                    <a href="">Tienda</a>
+                </li>
+                <li>
+                    <a href="">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
